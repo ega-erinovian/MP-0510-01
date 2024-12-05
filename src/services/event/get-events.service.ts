a7dd7d804b049129899f78e31698608123b6640b
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2024-12-06 00:07:17 +0700

    Events list (#5)
    * feat: add get all event data logic
    
    * feat: add join to related data with event table
    
    * fix: typo on variable naming

--- src/services/event/get-events.service.ts
+++ src/services/event/get-events.service.ts
@@ -2,8 +2,29 @@ import { prisma } from "../../lib/prisma";
 
 export const getEventsService = async () => {
   try {
-    const evenets = prisma.event.findMany();
-    return evenets;
+    const events = prisma.event.findMany({
+      include: {
+        organizer: {
+          select: {
+            id: true,
+            fullName: true,
+          },
+        },
+        city: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        category: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+    return events;
   } catch (error) {
     throw error;
   }
