4d9b5dd680b19c1517bde02d4b762c12b58da71e
ega-erinovian <erinovian69@gmail.com>
2024-12-05 23:52:37 +0700

    feat: add join to related data with event table

--- src/services/event/get-events.service.ts
+++ src/services/event/get-events.service.ts
@@ -2,7 +2,28 @@ import { prisma } from "../../lib/prisma";
 
 export const getEventsService = async () => {
   try {
-    const evenets = prisma.event.findMany();
+    const evenets = prisma.event.findMany({
+      include: {
+        organizer: {
+          select: {
+            id: true,
+            fullName: true,
+          },
+        },
+        city: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        category: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
     return evenets;
   } catch (error) {
     throw error;
