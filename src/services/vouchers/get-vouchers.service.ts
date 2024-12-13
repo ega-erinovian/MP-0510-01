fb8252373fe9298d1f7dec753eeaa21c197c5c1f
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2024-12-13 21:00:57 +0700

    Fix get vouchers (#11)
    * fix: change get vouchers to use react query
    
    * fix: give default value to boolean

--- src/services/vouchers/get-vouchers.service.ts
+++ src/services/vouchers/get-vouchers.service.ts
@@ -1,8 +1,38 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "../../lib/prisma";
+import { PaginationQueryParams } from "../../types/pagination";
 
-export const getVouchersService = async () => {
+interface GetVouchersQuery extends PaginationQueryParams {
+  search?: string;
+  eventId?: number;
+}
+
+export const getVouchersService = async (query: GetVouchersQuery) => {
   try {
-    const vouchers = prisma.voucher.findMany({
+    const { page, sortBy, sortOrder, take, search, eventId } = query;
+
+    const parsedEventId = eventId && Number(eventId);
+
+    const whereClause: Prisma.VoucherWhereInput = {};
+
+    if (parsedEventId) {
+      whereClause.eventId = parsedEventId; // Use parsed value
+    }
+
+    if (search) {
+      whereClause.OR = [
+        { code: { contains: search } },
+        { event: { title: { contains: search } } },
+      ];
+    }
+
+    const vouchers = await prisma.voucher.findMany({
+      where: whereClause,
+      skip: (page - 1) * take, // offset
+      take: take, // limit
+      orderBy: {
+        [sortBy]: sortOrder,
+      },
       include: {
         event: {
           select: {
@@ -16,7 +46,12 @@ export const getVouchersService = async () => {
         },
       },
     });
-    return vouchers;
+
+    const count = await prisma.voucher.count({
+      where: whereClause,
+    });
+
+    return { data: vouchers, meta: { page, take, total: count } };
   } catch (error) {
     throw error;
   }
