3fd6d01163e53cc0253ced07ab739a8029e4eb85
Ega Erinovian <72180780+ega-erinovian@users.noreply.github.com>
2024-12-23 13:13:42 +0700

    feat: add reset password (#21)
    * feat: add reset password
    
    * fix: build the feature

--- dist/controllers/auth.controller.js
+++ dist/controllers/auth.controller.js
@@ -9,10 +9,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.forgotPasswordController = exports.loginController = exports.registerController = void 0;
+exports.resetPasswordController = exports.forgotPasswordController = exports.loginController = exports.registerController = void 0;
 const login_service_1 = require("../services/auth/login.service");
 const register_service_1 = require("../services/auth/register.service");
 const forgot_password_service_1 = require("../services/auth/forgot-password.service");
+const reset_password_service_1 = require("../services/auth/reset-password.service");
 const registerController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     try {
@@ -45,3 +46,14 @@ const forgotPasswordController = (req, res, next) => __awaiter(void 0, void 0, v
     }
 });
 exports.forgotPasswordController = forgotPasswordController;
+const resetPasswordController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const userId = Number(res.locals.user.id);
+        const result = yield (0, reset_password_service_1.resetPasswordService)(userId, req.body.password);
+        res.status(200).send(result);
+    }
+    catch (error) {
+        next(error);
+    }
+});
+exports.resetPasswordController = resetPasswordController;
--- dist/lib/jwt.js
+++ dist/lib/jwt.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.verifyToken = void 0;
+exports.verifyTokenReset = exports.verifyToken = void 0;
 const env_1 = require("./env");
 const jsonwebtoken_1 = require("jsonwebtoken");
+const config_1 = require("../config");
 const verifyToken = (req, res, next) => {
     var _a;
     const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(" ")[1];
@@ -26,3 +27,26 @@ const verifyToken = (req, res, next) => {
     });
 };
 exports.verifyToken = verifyToken;
+const verifyTokenReset = (req, res, next) => {
+    var _a;
+    const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(" ")[1];
+    if (!token) {
+        res.status(401).send({
+            message: "Authentication failed. Token is missing",
+        });
+        return;
+    }
+    (0, jsonwebtoken_1.verify)(token, config_1.JWT_SECRET_FORGOT_PASSWORD, (err, payload) => {
+        if (err) {
+            if (err instanceof jsonwebtoken_1.TokenExpiredError) {
+                res.status(401).send({ message: "Token expired" });
+            }
+            else {
+                res.status(401).send({ message: "Invalid Token" });
+            }
+        }
+        res.locals.user = payload;
+        next();
+    });
+};
+exports.verifyTokenReset = verifyTokenReset;
