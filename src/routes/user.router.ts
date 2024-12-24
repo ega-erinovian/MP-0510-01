f601cf82d5cfe2ef3e83ccf43bc4fa9b3d1e4612
ega-erinovian <erinovian69@gmail.com>
2024-12-24 15:54:08 +0700

    feat: add edit profile

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import {
   checkReferralController,
+  getUserController,
   updateUserController,
 } from "../controllers/user.controller";
-import { validateUpdateUser } from "../validators/user.validator";
+import { uploader } from "../lib/multer";
+import { fileFilter } from "../lib/fileFilter";
+import { updateUserValidator } from "../validators/user.validator";
 
 const router = Router();
 
 router.get("/", checkReferralController);
-router.patch("/:id", validateUpdateUser, updateUserController);
+router.get("/:id", getUserController);
+router.patch(
+  "/:id",
+  uploader().fields([{ name: "profilePicture", maxCount: 1 }]),
+  fileFilter,
+  updateUserController
+);
 
 export default router;
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,25 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
-export const validateUpdateUser = [
-  body("fullName").optional().isString().withMessage("Name must be a string"),
-  body("email").optional().isEmail().withMessage("Email must be a valid email"),
-  body("password")
+export const updateUserValidator = [
+  // Validate body fields (you can add more validation rules based on your schema)
+  body("fullName")
     .optional()
     .isString()
-    .withMessage("Password must be a string"),
-  body("phoneNumber").optional(),
-  body("cityId").optional().isNumeric().withMessage("City ID must be a number"),
-  body("point").optional().isNumeric().withMessage("Point must be a number"),
+    .withMessage("Full name must be a string"),
+  body("email").optional().isEmail().withMessage("Email must be valid"),
+  body("phoneNumber")
+    .optional()
+    .isString()
+    .withMessage("Phone number must be a string"),
 
+  // Custom validation to handle errors after checking all rules
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
-      res.status(400).send({ message: errors.array()[0].msg });
-      return;
+      return res.status(400).json({ errors: errors.array() });
     }
-
     next();
   },
 ];
